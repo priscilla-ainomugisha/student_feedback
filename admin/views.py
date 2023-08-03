4ceacef8c7dc92a3d0310c300654b8b2cd2c9505
cr17 <charlesrolandscr17@gmail.com>
2023-08-03 17:08:29 +0300

    Fixed admin

--- admin/views.py
+++ admin/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def signin(request):
+    return render(request, "signin.html")
